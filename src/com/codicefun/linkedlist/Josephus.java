6404831d97d2221730f98c7fb9a72a32dc29be0e
James Wang <james_wangx@163.com>
2023-01-25 19:53:35 +0800

    解决约瑟夫问题

--- src/com/codicefun/linkedlist/Josephus.java
+++ src/com/codicefun/linkedlist/Josephus.java
@@ -3,8 +3,11 @@ package com.codicefun.linkedlist;
 public class Josephus {
     public static void main(String[] args) {
         CircleSingleLinkedList list = new CircleSingleLinkedList();
-        list.addNode(5);
+        list.addBoy(5);
         list.list();
+
+        System.out.println();
+        list.countBoy(3, 7);
     }
 }
 
@@ -12,30 +15,30 @@ public class Josephus {
  * 环形单向链表
  */
 class CircleSingleLinkedList {
-    private Node first;
+    private Boy first;
 
     /**
-     * 添加 n 个节点
+     * 添加 n 个小孩
      *
-     * @param n 节点个数
+     * @param n 小孩个数
      */
-    public void addNode(int n) {
+    public void addBoy(int n) {
         if (n < 2) {
             System.out.println("nums 值不正确");
             return;
         }
 
-        Node curNode = null;
+        Boy curBoy = null;
         for (int i = 1; i <= n; i++) {
-            Node node = new Node(i);
+            Boy boy = new Boy(i);
             if (i == 1) {
-                first = node;
-                node.setNext(first);
-                curNode = first;
+                first = boy;
+                boy.next = first;
+                curBoy = first;
             } else {
-                curNode.setNext(node);
-                node.setNext(first);
-                curNode = node;
+                curBoy.next = boy;
+                boy.next = first;
+                curBoy = boy;
             }
         }
     }
@@ -49,38 +52,56 @@ class CircleSingleLinkedList {
             return;
         }
 
-        Node curNode = first;
+        Boy curBoy = first;
         while (true) {
-            System.out.print(curNode.getNo() + " ");
-            if (curNode.getNext() == first) {
+            System.out.print(curBoy.no + " ");
+            if (curBoy.next == first) {
                 break;
             }
-            curNode = curNode.getNext();
+            curBoy = curBoy.next;
         }
+    }
+
+    /**
+     * 小孩报数
+     *
+     * @param start 开始的小孩
+     * @param n     报数的次数
+     */
+    public void countBoy(int start, int n) {
+        if (first == null) {
+            System.out.println("链表为空");
+            return;
+        }
+
+        Boy curBoy = first;
+        // 报数的次数
+        int t = 0;
+        while (curBoy.next != curBoy) {
+            if (curBoy.no == start) {
+                t = 1; // 开始第轮次报数
+                // 找到起始位置后修改 start，防止下次循环再次到达该位置重置 t
+                start = -1;
+            } else {
+                t++; // 接着报数
+            }
+            if (t == n - 1) { // 找到将要出列的前一个孩子
+                System.out.print(curBoy.next.no + " ");
+                curBoy.next = curBoy.next.next;
+                start = curBoy.next.no; // 开始下轮次报数
+            }
+            curBoy = curBoy.next;
+        }
+
+        System.out.print(curBoy.no + " ");
     }
 }
 
-class Node {
-    private int no;
-    private Node next;
+class Boy {
+    public int no;
+    public Boy next;
 
-    public Node(int no) {
+    public Boy(int no) {
         this.no = no;
-    }
-
-    public int getNo() {
-        return no;
-    }
-
-    public void setNo(int no) {
-        this.no = no;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
     }
 }
