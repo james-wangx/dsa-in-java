663d4429a6fd09ab5ec6ccc20fdf73ed57e246c9
James Wang <james_wangx@163.com>
2023-02-09 21:32:33 +0800

    哈夫曼编码与解码

--- src/com/codicefun/huffmancode/HuffmanCode.java
+++ src/com/codicefun/huffmancode/HuffmanCode.java
@@ -1,179 +1,193 @@
 package com.codicefun.huffmancode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * 哈夫曼编码
+ * 将一段文本转为哈夫曼编码
+ * 1、将文本转为字节数组
+ * 2、遍历数组，统计单个字节出现次数，记录权重，转为节点
  */
-public class HuffmanCode {
-    // 生成哈夫曼编码
-    // 思路：
-    // 1、将哈夫曼编码存放在 Map<Bytes, String> 形式
-    static Map<Byte, String> huffmanCodes = new HashMap<>();
-    // 2、在生成哈夫曼编码时，需要拼接路径，定义一个 StringBuilder 存储某个叶子节点的路径
-    static StringBuilder sb = new StringBuilder();
+class HuffmanCode {
+    // 编码本
+    private static HashMap<Byte, String> codeMap = new HashMap<>();
 
-    public static void main(String[] args) {
-        String content = "i like like like java do you like a java";
-        byte[] hc = huffmanZip(content.getBytes());
-
-        System.out.println(Arrays.toString(hc));
+    public static HashMap<Byte, String> getCodeMap() {
+        return codeMap;
     }
 
-    // 封装哈夫曼编码方法
-    public static byte[] huffmanZip(byte[] bytes) {
-        List<Node> nodes = getNodes(bytes);
-        Node root = createHuffmanTree(nodes);
-        Map<Byte, String> hc = getCodes(root);
-
-        return zip(bytes, hc);
+    public static void setCodeMap(HashMap<Byte, String> codeMap) {
+        HuffmanCode.codeMap = codeMap;
     }
 
     /**
-     * 生成最终的哈夫曼编码
+     * 获取字节出现的频率
      *
-     * @param bytes 与原始字符串对应的字符数组
-     * @param hc    哈夫曼编码集合
-     * @return 哈夫曼编码数组
+     * @param bytes 原始字节数组
+     * @return HashMap<Byte, Integer> key 为字节，value 为频率
      */
-    private static byte[] zip(byte[] bytes, Map<Byte, String> hc) {
-        StringBuilder sb = new StringBuilder();
+    private static HashMap<Byte, Integer> getByteFrequency(byte[] bytes) {
+        HashMap<Byte, Integer> byteFrequency = new HashMap<>();
 
         for (byte b : bytes) {
-            sb.append(hc.get(b));
+            byteFrequency.merge(b, 1, Integer::sum);
         }
 
-        // 将字符串转为字节数组
-        int len = (sb.length() + 7) / 8;
-        byte[] hb = new byte[len];
-        int index = 0; // 记录数组索引
-
-        for (int i = 0; i < sb.length(); i += 8) {
-            String substring;
-            if (i + 8 > sb.length()) {
-                substring = sb.substring(i);
-            } else {
-                substring = sb.substring(i, i + 8);
-            }
-            hb[index++] = (byte) Integer.parseInt(substring, 2);
-        }
-
-        return hb;
+        return byteFrequency;
     }
 
     /**
-     * 将 bytes 数组转为 Node 列表
+     * 获取节点列表
      *
-     * @param bytes 字节数组
-     * @return Node 列表
+     * @param frequency 字节出现频率
+     * @return 节点列表
      */
-    private static List<Node> getNodes(byte[] bytes) {
-        // 1、创建 ArrayList
-        List<Node> nodes = new ArrayList<>();
+    private static List<Node> getNodeList(HashMap<Byte, Integer> frequency) {
+        ArrayList<Node> nodeList = new ArrayList<>();
+        frequency.forEach((k, v) -> nodeList.add(new Node(k, v)));
 
-        // 存储每一个 byte 出现的次数
-        HashMap<Byte, Integer> counts = new HashMap<>();
-        for (byte b : bytes) {
-            counts.merge(b, 1, Integer::sum); // 方法引用
-        }
-
-        // 把一个键值对转成一个 Node 对象，并加入 nodes 集合
-        for (Map.Entry<Byte, Integer> entry : counts.entrySet()) {
-            nodes.add(new Node(entry.getKey(), entry.getValue()));
-        }
-
-        return nodes;
-    }
-
-    // 创建哈夫曼树
-    private static Node createHuffmanTree(List<Node> nodes) {
-        while (nodes.size() > 1) {
-            Collections.sort(nodes);
-            Node leftNode = nodes.get(0);
-            Node rightNode = nodes.get(1);
-            // 父节点没有 data 编码
-            Node parent = new Node(null, leftNode.weight + rightNode.weight);
-            parent.left = leftNode;
-            parent.right = rightNode;
-            nodes.remove(leftNode);
-            nodes.remove(rightNode);
-            nodes.add(parent);
-        }
-
-        return nodes.get(0);
-    }
-
-    private static Map<Byte, String> getCodes(Node root) {
-        if (root == null) {
-            return null;
-        }
-
-        getCodes(root.left, "0", sb);
-        getCodes(root.right, "1", sb);
-
-        return huffmanCodes;
+        return nodeList;
     }
 
     /**
-     * 得到哈夫曼编码并放入 huffmanCodes
+     * 创建编码本
      *
      * @param node 节点
-     * @param code 路径：左子节点是 0，右子节点是 1
-     * @param sb   用于拼接路径
+     * @param str  路径，左节点为 0，右节点为 1
+     * @param code 当前节点的编码
      */
-    private static void getCodes(Node node, String code, StringBuilder sb) {
-        StringBuilder sb2 = new StringBuilder(sb);
-        sb2.append(code);
+    private static void createCodeMap(Node node, String str, StringBuilder code) {
+        // 当前节点的编码
+        StringBuilder sb = new StringBuilder();
+        sb.append(code).append(str);
 
-        if (node != null) {
-            // 判断当前 node 是叶子节点还是非叶子节点
-            if (node.data == null) {
-                // 向左递归
-                getCodes(node.left, "0", sb2);
-                // 向右递归
-                getCodes(node.right, "1", sb2);
+        if (node.data == null) {
+            // 当前节点是非叶子节点
+            if (node.left != null) {
+                createCodeMap(node.left, "0", sb);
+            }
+            if (node.right != null) {
+                createCodeMap(node.right, "1", sb);
+            }
+        } else {
+            // 当前节点是叶子节点
+            codeMap.put(node.data, sb.toString());
+        }
+    }
+
+    /**
+     * 编码的最后一步，按照编码本和原始文本生成编码后的二进制字符串，再将其转为字节数组
+     *
+     * @param content 原始字节数组
+     * @return 编码后的字节数组
+     */
+    private static byte[] getCode(byte[] content) {
+        StringBuilder sb = new StringBuilder();
+
+        for (byte b : content) {
+            sb.append(codeMap.get(b));
+        }
+
+        int len = (sb.length() + 7) / 8; // 最小长度
+        byte[] code = new byte[len]; // 编码数组
+        // i 为字符串索引，j 为编码数组索引
+        for (int i = 0, j = 0; j < len; i += 8, j++) {
+            if (i + 8 > sb.length()) {
+                code[j] = (byte) Integer.parseInt(sb.substring(i), 2);
             } else {
-                // 叶子节点，停止遍历
-                huffmanCodes.put(node.data, sb2.toString());
+                code[j] = (byte) Integer.parseInt(sb.substring(i, i + 8), 2);
             }
         }
-    }
-}
 
-class Node implements Comparable<Node> {
-    Byte data; // 存放数据本身
-    int weight; // 权值
-    Node left;
-    Node right;
-
-    public Node(Byte data, int weight) {
-        this.data = data;
-        this.weight = weight;
+        return code;
     }
 
-    // 前序遍历
-    public void preOrder() {
-        System.out.println(this);
+    /**
+     * 编码
+     *
+     * @param content 原始文本内容
+     * @return 编码后的字节数组
+     */
+    public static byte[] code(String content) {
+        // 1、将文本转为字节数组
+        byte[] bytes = content.getBytes();
+        // 2、计算字节出现次数（频率）
+        HashMap<Byte, Integer> frequency = getByteFrequency(bytes);
+        // 3、根据频率生成节点（字节为 data，频率为 weights 即权重）
+        List<Node> nodeList = getNodeList(frequency);
+        // 4、生成哈夫曼树，得到根节点
+        Node rootNode = HuffmanTree.createTree(nodeList);
+        // 5、生成编码本
+        createCodeMap(rootNode, "", new StringBuilder());
 
-        if (left != null) {
-            left.preOrder();
+        // 6、获得编码，并返回
+        return getCode(bytes);
+    }
+
+    /**
+     * 将字节数组转为二进制字符串
+     *
+     * @param codes 编码后的字节数组
+     * @return 二进制字符串
+     */
+    private static String getBinaryString(byte[] codes) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < codes.length; i++) {
+            String str;
+            String format;
+            if (i == codes.length - 1) {
+                // 如果是最后一个字节，不需要补位
+                str = Integer.toBinaryString(codes[i]);
+                format = str;
+            } else {
+                str = Integer.toBinaryString(codes[i] | 0x100);
+                format = str.substring(str.length() - 8);
+            }
+            sb.append(format);
         }
 
-        if (right != null) {
-            right.preOrder();
+        return sb.toString();
+    }
+
+    /**
+     * 将编码的二进制字符串转为原始文本
+     *
+     * @param codes 编码后的二进制字符串
+     * @return 原始文本
+     */
+    private static String getContent(String codes) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder content = new StringBuilder();
+        HashMap<String, Byte> decodeMap = new HashMap<>();
+
+        // 生成解码本
+        codeMap.forEach((key, value) -> decodeMap.put(value, key));
+
+        int index = 0;
+        while (index < codes.length()) {
+            sb.append(codes.charAt(index++));
+            Byte b = decodeMap.get(sb.toString());
+            if (b != null) {
+                content.append((char) (byte) b);
+                sb = new StringBuilder();
+            }
         }
+
+        return content.toString();
     }
 
-    @Override
-    public String toString() {
-        return "Node{" +
-                "data=" + data +
-                ", weight=" + weight +
-                '}';
-    }
+    /**
+     * 解码
+     *
+     * @param codes 编码后的字节数组
+     * @return 原始文本内容
+     */
+    public static String decode(byte[] codes) {
+        String binaryString = getBinaryString(codes);
 
-    @Override
-    public int compareTo(Node o) {
-        return this.weight - o.weight;
+        return getContent(binaryString);
     }
 }
---
+++ src/com/codicefun/huffmancode/HuffmanTree.java
@@ -0,0 +1,31 @@
+package com.codicefun.huffmancode;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 哈夫曼树
+ */
+public class HuffmanTree {
+    /**
+     * 生成哈夫曼树
+     *
+     * @param nodeList 节点列表
+     * @return 根节点
+     */
+    public static Node createTree(List<Node> nodeList) {
+        while (nodeList.size() > 1) {
+            Collections.sort(nodeList);
+            Node leftNode = nodeList.get(0);
+            Node rightNode = nodeList.get(1);
+            Node parentNode = new Node(null, leftNode.weights + rightNode.weights);
+            parentNode.left = leftNode;
+            parentNode.right = rightNode;
+            nodeList.remove(leftNode);
+            nodeList.remove(rightNode);
+            nodeList.add(parentNode);
+        }
+
+        return nodeList.get(0);
+    }
+}
