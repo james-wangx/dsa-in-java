8cf83aa4ca3097aefcbb43babe843dc57079cec3
James Wang <james_wangx@163.com>
2023-02-11 13:41:46 +0800

    查找目标节点和父节点

--- src/com/codicefun/binarysorttree/Node.java
+++ src/com/codicefun/binarysorttree/Node.java
@@ -46,6 +46,49 @@ public class Node {
         }
     }
 
+    /**
+     * 查找目标节点
+     *
+     * @param value 待查找节点的值
+     * @return 目标节点或 null
+     */
+    protected Node search(int value) {
+        if (this.value == value) { // 当前节点
+            return this;
+        } else if (value < this.value) {// 在左子树
+            if (left == null) {// 左节点不存在
+                return null;
+            }
+            return left.search(value);// 向左递归
+        } else {// 在右子树
+            if (right == null) {// 右节点不存在
+                return null;
+            }
+            return right.search(value); // 向右递归
+        }
+    }
+
+    /**
+     * 查找目标节点的父节点
+     *
+     * @param value 目标节点的值
+     * @return 目标节点的父节点或 null
+     */
+    protected Node searchParent(int value) {
+        if ((left != null && left.value == value)
+                || (right != null && right.value == value)) { // 当前节点
+            return this;
+        } else {
+            if (value < this.value && left != null) { // 在左子树
+                return left.searchParent(value); // 向左递归
+            } else if (value > this.value && right != null) { // 在右子树
+                return right.searchParent(value); // 向右递归
+            } else { // 没有父节点
+                return null;
+            }
+        }
+    }
+
     @Override
     public String toString() {
         return String.format("{value=%d}", value);
