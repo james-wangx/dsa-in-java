42d632f0fb4c3d06f8a95a2374a82252951c03ab
James Wang <james_wangx@163.com>
2023-02-11 17:58:22 +0800

    删除

--- src/com/codicefun/binarysorttree/BinarySortTree.java
+++ src/com/codicefun/binarysorttree/BinarySortTree.java
@@ -10,16 +10,13 @@ public class BinarySortTree {
      * 根据数组创建树
      *
      * @param arr 数组
-     * @return 根节点
      */
-    public Node createTree(int[] arr) {
+    public void createTree(int[] arr) {
         root = new Node(arr[0]);
 
         for (int i = 1; i < arr.length; i++) {
             root.add(new Node(arr[i]));
         }
-
-        return root;
     }
 
     public void add(Node node) {
@@ -27,6 +24,84 @@ public class BinarySortTree {
             root = node;
         } else {
             root.add(node);
+        }
+    }
+
+    /**
+     * 删除节点
+     *
+     * @param value 待删除节点的值
+     */
+    public void del(int value) {
+        // 空树
+        if (root == null) {
+            return;
+        }
+
+        // 未找到节点
+        Node targetNode = root.search(value);
+        if (targetNode == null) {
+            return;
+        }
+
+        // 是根节点且仅此一个节点
+        if (root.left == null && root.right == null) {
+            root = null;
+            return;
+        }
+
+        Node parentNode = root.searchParent(value);
+        // 叶子节点
+        if (targetNode.left == null && targetNode.right == null) {
+            // 是父节点的左节点
+            if (parentNode.left != null && parentNode.left == targetNode) {
+                parentNode.left = null;
+                return;
+            }
+            // 是父节点的右节点
+            if (parentNode.right != null && parentNode.right == targetNode) {
+                parentNode.right = null;
+            }
+            // 有两颗子树的节点
+        } else if (targetNode.left != null && targetNode.right != null) {
+            Node temp = targetNode.right;
+            while (temp.left != null) {
+                temp = temp.left;
+            }
+            del(temp.value);
+            targetNode.value = temp.value;
+            // 只有一颗子树的节点
+        } else {
+            // 是根节点
+            if (parentNode == null) {
+                // 只有左节点
+                if (targetNode.left != null) {
+                    root = targetNode.left;
+                    // 只有右节点
+                } else {
+                    root = targetNode.right;
+                }
+                return;
+            }
+            // 只有左节点
+            if (targetNode.left != null) {
+                // 是父节点的左节点
+                if (parentNode.left == targetNode) {
+                    parentNode.left = targetNode.left;
+                    // 是父节点的右节点
+                } else {
+                    parentNode.right = targetNode.left;
+                }
+                // 只有右节点
+            } else {
+                // 是父节点的左节点
+                if (parentNode.left == targetNode) {
+                    parentNode.left = targetNode.right;
+                    // 是父节点的右节点
+                } else {
+                    parentNode.right = targetNode.right;
+                }
+            }
         }
     }
 
--- test/TestBinarySortTree.java
+++ test/TestBinarySortTree.java
@@ -15,4 +15,36 @@ public class TestBinarySortTree {
         String order = tree.infixOrder();
         assert order.equals("1 2 3 5 7 9 10 12 ");
     }
+
+    @Test
+    public void testDel() {
+        // 叶子节点
+        tree.createTree(arr);
+        tree.del(2);
+        assert tree.infixOrder().equals("1 3 5 7 9 10 12 ");
+        tree.del(12);
+        assert tree.infixOrder().equals("1 3 5 7 9 10 ");
+
+        // 仅有一颗子树的节点
+        tree.del(10);
+        assert tree.infixOrder().equals("1 3 5 7 9 ");
+        tree.createTree(arr);
+        tree.del(1);
+        assert tree.infixOrder().equals("2 3 5 7 9 10 12 ");
+        tree.createTree(new int[]{2, 4, 3, 5});
+        tree.del(2);
+        assert tree.infixOrder().equals("3 4 5 ");
+        tree.createTree(new int[]{5, 3, 2, 4});
+        tree.del(5);
+        assert tree.infixOrder().equals("2 3 4 ");
+
+        // 有两颗子树的节点
+        tree.createTree(arr);
+        tree.del(10);
+        assert tree.infixOrder().equals("1 2 3 5 7 9 12 ");
+        tree.del(3);
+        assert tree.infixOrder().equals("1 2 5 7 9 12 ");
+        tree.del(7);
+        assert tree.infixOrder().equals("1 2 5 9 12 ");
+    }
 }
