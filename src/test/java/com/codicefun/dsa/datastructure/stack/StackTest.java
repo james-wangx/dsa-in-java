ed147d7721b138279213bf75a6b4c71b0204d8b9
James Wang <james_wangx@163.com>
2024-03-14 21:47:06 +0800

    Fix stack test

--- src/test/java/com/codicefun/dsa/datastructure/stack/StackTest.java
+++ src/test/java/com/codicefun/dsa/datastructure/stack/StackTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.NoSuchElementException;
-import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
